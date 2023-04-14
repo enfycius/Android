8c8fa1554cd7820480131575f0d0def21b56fe7d
enfycius <hexk0131@gmail.com>
2023-04-14 15:51:39 +0900

    Another Activity

---
+++ Tutorial_4/app/src/main/java/com/example/anotheractivity/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.anotheractivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(v.getContext(), SecondActivity.class));
+            }
+        });
+    }
+
+}
